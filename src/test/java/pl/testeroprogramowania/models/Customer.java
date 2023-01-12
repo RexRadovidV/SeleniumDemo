212a1045dc33b14fc15d94fd3c3ed82b711b05f0
Wojtek Rachoń <wojtek.rachon@gmail.com>
2023-01-12 12:33:19 +0100

    add tests and page object

---
+++ src/test/java/pl/testeroprogramowania/models/Customer.java
@@ -0,0 +1,95 @@
+package pl.testeroprogramowania.models;
+
+public class Customer {
+
+    private String firstName="Wojciech";
+    private String lastName="Rachoń";
+    private String companyName="My";
+    private String country="Poland";
+    private String street="Zakrzówek Wieś";
+    private String flatNumber="133";
+    private String zipCode="23-213";
+    private String city="Zakrzówek";
+    private String phone="784452586";
+    private String email="awordaday12@gmail.com";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/test/java/pl/testeroprogramowania/tests/CheckoutTest.java
@@ -0,0 +1,28 @@
+package pl.testeroprogramowania.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pl.testeroprogramowania.models.Customer;
+import pl.testeroprogramowania.pages.HomePage;
+import pl.testeroprogramowania.pages.OrderDetailsPage;
+
+public class CheckoutTest extends BaseTest {
+
+    @Test
+    public void checkoutTest() {
+
+        Customer customer = new Customer();
+
+        OrderDetailsPage orderDetailsPage = new HomePage(driver)
+                .openShopPage()
+                .openProduct("Java Selenium WebDriver")
+                .addProductToCart()
+                .viewCart()
+                .openAddressDetails()
+                .fillAddressDetails(customer, "Some random comment");
+
+        Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(), "Thank you. Your order has been received.");
+        Assert.assertTrue(orderDetailsPage.getProductName().getText().contains("Java Selenium WebDriver"));
+    }
+
+}
